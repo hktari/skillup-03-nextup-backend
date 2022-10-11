127de080e3892313a239a037eb9bbd870b009a89
hktari <bkamnik1995@gmail.com>
2022-10-11 12:17:35 +0200

    add booking seed

--- api/src/db/data/seed/user.seeder.ts
+++ api/src/db/data/seed/user.seeder.ts
@@ -2,19 +2,28 @@ import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { DataSource } from 'typeorm';
 import { User } from '../../../user/entities/user.entity';
 import { Event } from '../../../event/entities/event.entity'
+import { Booking } from '../../../booking/entities/booking.entity';
 export default class UserSeeder implements Seeder {
   public async run(
     dataSource: DataSource,
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
-    console.log('users ....');
-
     const userFactory = factoryManager.get<User>(User)
     await userFactory.saveMany(5);
 
     const eventFactory = factoryManager.get<Event>(Event)
-    await userFactory.save({
-      events: await eventFactory.saveMany(5)
+    const existingUser = await userFactory.save({
+      email: 'existing.user@example.com',
+      events: await eventFactory.saveMany(4)
     })
+
+    const existingEvent = await eventFactory.save({
+      title: 'existing event',
+      user: existingUser
+    })
+
+    const bookingRepository = dataSource.getRepository<Booking>(Booking)
+
+    await bookingRepository.save({ event: existingEvent, user: existingUser })
   }
 }

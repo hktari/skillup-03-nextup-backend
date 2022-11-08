c72a199d67e18b7e5728b6721d6e89621cac8a9d
hktari <bkamnik1995@gmail.com>
2022-11-08 09:27:49 +0100

    implement reminder logic

--- reminder-service/src/main.ts
+++ reminder-service/src/main.ts
@@ -1,23 +1,49 @@
 import logger from './logger'
 import ConfigService from './lib/config.service';
 import { EmailService } from './lib/email.service';
-import EventService from './lib/event.service';
+import DatabaseService from './lib/database.service';
+
+
 
 async function main() {
     logger.info('reminder service start');
 
     const config = new ConfigService()
     const emailService = new EmailService(config)
-    const eventService = new EventService(config)
+    const dbService = new DatabaseService(config)
     // emailService.sendEventReminder('bkamnik1995@gmail.com')
     // todo:
     // 1. connect to the database and get all events which are to occur tomorrow
     // 2. for each event, get all the bookings
-    // 3. for each booking, check if an email hasn't been sent yet, send email reminding the user about the event
 
     logger.debug('retrieving events...')
-    const events = await eventService.getEvents()
-    logger.debug(JSON.stringify(events))
+    const events = await dbService.getEvents()
+    const today = new Date()
+    const tomorrow = new Date()
+    tomorrow.setDate(today.getDate() + 2)
+    tomorrow.setUTCHours(0)
+    tomorrow.setUTCMinutes(0)
+
+    const sendEmailPromises = []
+    for (const event of events.documents) {
+        logger.debug(`considering event: id: ${event.eventId}\ttitle:${event.title}`)
+        const eventDatetime = new Date(event.datetime)
+        logger.debug('event datetime: ' + eventDatetime)
+        if (eventDatetime >= today && eventDatetime <= tomorrow) {
+            logger.info('pending event tomorrow')
+
+            const bookingsPendingReminder = await dbService.getBookingsForEventPendingReminder(event.eventId)
+            logger.debug(`queueing ${bookingsPendingReminder.documents.length} emails to be sent`)
+            for (const booking of bookingsPendingReminder.documents) {
+                sendEmailPromises.push(emailService.sendEventReminder(booking.user[0].email))
+            }
+        }
+    }
+
+    logger.info('sending emails...')
+
+    const results = await Promise.allSettled(sendEmailPromises)
+
     logger.info('reminder service end');
 }
 
